4b85a589e1e5aedee905f2cc09d9bc7c447494f1
Drew Hendrickson <dhendrickson@outlook.com>
2013-05-28 17:06:56 +0930

    cleaned up MainPage fcn

--- backend.py
+++ backend.py
@@ -30,26 +30,6 @@ class DataObject(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greetings_query = Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-        greetings = greetings_query.fetch(10)
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.urlencode({'guestbook_name': guestbook_name}),
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
