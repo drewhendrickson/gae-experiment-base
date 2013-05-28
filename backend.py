6fb8e3c35a1a5756378ab624fe0f019172e25245
Drew Hendrickson <dhendrickson@outlook.com>
2013-05-28 17:08:09 +0930

    cleanup code

--- backend.py
+++ backend.py
@@ -7,7 +7,6 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
