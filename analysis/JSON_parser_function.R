aeadab721365ffdd1bd6b94599755099bb56f2bb
Drew Hendrickson <dhendrickson@outlook.com>
2013-07-16 18:59:03 +0930

    added total size to output

--- analysis/JSON_parser_function.R
+++ analysis/JSON_parser_function.R
@@ -2,6 +2,7 @@ parseJSONlist <- function(json) {
   require(RJSONIO)
   require(plyr)
   all <- as.data.frame(fromJSON(I(json[1])))
+  print(paste("total rows:", nrow(json)))
   
   for (i in 2:length(json)) {
     if (i %% 100 == 0) { print(i) }
