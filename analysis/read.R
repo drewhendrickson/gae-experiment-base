6c7f9a23464ef3c677ed00b003c708bb10587cb9
Drew Hendrickson <dhendrickson@outlook.com>
2013-07-16 18:58:52 +0930

    fixend naming bug.

--- analysis/read.R
+++ analysis/read.R
@@ -3,8 +3,8 @@ raw_gae_data <- read.csv(file="raw_data.csv", header=T)
 
 source('JSON_parser_function.R')
 # this can take 10+ minutes
-d <- parseJSONlist(raw_gae_turk_data[,1])
+d <- parseJSONlist(raw_gae_data[,1])
 
-gae_turk_data <- cbind(raw_gae_turk_data[,2:3], d)
+gae_data <- cbind(raw_gae_data[,2:3], d)
 
-save(gae_turk_data, file="gae_data.RData")
+save(gae_data, file="gae_data.RData")
