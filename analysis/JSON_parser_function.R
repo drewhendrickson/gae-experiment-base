4e7c4fd31b49b929a6bbd4c9cf2e3db005f80de1
Drew Hendrickson <dhendrickson@outlook.com>
2013-08-27 11:37:50 +0930

    significant speedup and bug fix to reading csv of json

--- analysis/JSON_parser_function.R
+++ analysis/JSON_parser_function.R
@@ -1,18 +1,34 @@
+require(RJSONIO)
+require(plyr)
+
 parseJSONlist <- function(json) {  
-  require(RJSONIO)
-  require(plyr)
-  all <- as.data.frame(fromJSON(I(json[1])))
-  print(paste("total rows:", nrow(json)))
+  print(paste("total rows to parse:", length(json)))
+
+  # read the first row to know the number of columns
+  first_row <- data.frame(fromJSON(I(json[1])), 
+                          stringsAsFactors=FALSE)
   
+  # initialize the data frame for all data to be written
+  # pre-allocate all entries with NAs that will be overwritten
+  all <- data.frame(matrix(NA, nrow=length(json), ncol=ncol(first_row)), 
+                    stringsAsFactors=FALSE)
+  names(all) <- names(first_row)
+  all[1,] <- first_row
+  
+  # read and parse all remaining rows
   for (i in 2:length(json)) {
-    if (i %% 100 == 0) { print(i) }
+    if (i %% 100 == 0) { print(paste("Processing row:", i)) }
     
     t = json[i]
-    t2 = fromJSON(I(t))
+    t2 = data.frame(fromJSON(I(t)), stringsAsFactors=FALSE)
+        
+    # check for any row length issues:
+    if (ncol(t2) != ncol(all)) {
+      print("ERROR! ENTRY", i, "NOT OF THE SAME LENGTH AS OTHER ROWS.")
+    }
     
-    tmp <- as.data.frame(t2)
-    
-    all <- rbind.fill(tmp, all)
+    # add this row to the full data set
+    all[i,] <- t2
   }
   
   return (all)  
