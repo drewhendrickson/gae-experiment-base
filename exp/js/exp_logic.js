0d732916f99c4fa28e12b3ba0a8c3ba848d1ebbf
Drew Hendrickson <dhendrickson@gmail.com>
2014-06-27 12:14:21 +0930

    improve comments and structure more cleanly

--- exp/js/exp_logic.js
+++ exp/js/exp_logic.js
@@ -1,60 +1,70 @@
 /*global $, console, initializeCanvas, initializeSlider, hideElements, showInputOptions, showIntro, saveData */
 
-// participant variables
+// variables that will store information about the subject and the experiment condition
 var subjectID;
 var condition;
 var demographics = [];
 
-// experimental variables 
+// variables that will store the current trial number and current block number
 var currTrial = 0;
 var currBlock = 0;
 
-// canvas variables
+// variables that will store references to the html canvas used to display stimuli
 var context;
 var canvas;
 
-// response variable
-var response;
+// variables that will store what response subjects select and response time information
+var response, base_time, rt;
 
-// timing variables
-var base_time, rt;
-
-// SLIDER
-// slider variables
-var default_slider_value = 50;
+// the default value of the slider
+var default_slider_value;
 
 // references to divs in the html
 var divImageSpace, divSlider, divInstructions, divBlue, divGreen, divSliderStuff, divSliderInfo, divNext;
 
-
 /* Variables you likely will need to change are below */
 
+// how many test trials to do per block
+var maxTestTrial = 5;
+
+// how many blocks to do total
+var maxBlock = 2;
+
+// set this to false if you want the user to determine which condition to start in
+// set this to true if you want to randomize the condition
+var randomizeConditions = false;
+
+// experimental stimuli
+// in this case, the angle of the line to draw (in degrees)
 var trainTrialStimuli = [130, -130, -20, 50, -10, -20, 70, 170, 120, 100, -120, 10, -30, 160, 140];
 var testTrialStimuli = [160, -150, 120, -50, -150, 130, -80, -10, -40, 170, -120, 20, 20, -50, -170];
 
-// CONDITION 
-// experimental conditions
-var colourCondition;
-
-
-// begin the experiment, initialize canvas, slider, subjectID, 
 function start () {
+  /* 
+  * start is the first function called (from init_exp.js) when all the files are loaded
+  * this function initializes many things, such as:
+  * a bunch of javascript objects, the canvas we will draw on, the slider, and the subject id
+  *
+  * if radomizeConditions is set to false the user can select which condition they will see
+  * and which segment of the experiment they will start in
+  * otherwise the condition is randomized and 
+  * this function finishes by calling showIntro to begin the experiment
+  */
+  
   // initialize references to elements in html
   initDivReferences();
 
-  // initialize canvas drawing
+  // initialize html canvas object
   initializeCanvas();
 
   // initialize the slider
   initializeSlider(100);
 
-  hideElements();
-
   // generate a subject ID by generating a random number between 1 and 1000000
   subjectID = Math.round(Math.random() * 1000000);
 
-  if (false) {
-    // allow user to select where to start the experiment
+  // if you set this to true, it allow user to select conditions and where to start
+  if (randomizeConditions) {
     showInputOptions();
   } else {
     // randomize experimental conditions
@@ -64,6 +74,15 @@ function start () {
 }
 
 function initDivReferences () {
+  /* 
+  * initDivReferences makes a number of jQuery requests to get html elements
+  * the results are stored in javascript variables to make accessing them much faster
+  * it does not return any value when finished
+  *
+  * you might want to add new js variables here if you add new html elements
+  * to your index.html file. Be sure you declare new ones as is done above for these variables
+  */
+  
   divImageSpace = $('#imageSpace');
   divInstructions = $('#instructions');
 
@@ -77,18 +96,37 @@ function initDivReferences () {
 }
 
 function initializeCondition () {
+  /* 
+  * initializeCondition randomly determines which condition to run the current user
+  * it does not return any value when finished
+  *
+  * if you have more than 2 conditions, you might want to add more functionality here
+  */
+  
   var r = Math.ceil(Math.random() * 2); // generate random number
   if (r === 1) {
-    colourCondition = 'red';
+    condition = 'red';
   } else if (r === 2) {
-    colourCondition = 'blue';
+    condition = 'blue';
   }
 }
 
-
-// draw experimental stimuli using canvas functions
 function drawLine(degrees, colour, width, height) {
+  /*
+  * drawLine draws a line in the html canvas
+  * it assumes the canvas is already initialized
+  * this function does not return any value when finished
+  * 
+  * degrees: the angle of the line
+  * colour: the color of the line
+  * width: the width of the canvas to draw on
+  * height: the height of hte canvas to draw on
+  */
+  
+  // convert degrees to radians
   var radians = degrees * (Math.PI / 180);
+
+  // the length of the line
   var length = 200;
 
   // set width of line
@@ -106,6 +144,25 @@ function drawLine(degrees, colour, width, height) {
 }
 
 function saveTestTrial() {
+  /*
+  * saveTestTrial builds up an object (exp_data) containing all of the
+  * data from the current trial to save to the database
+  * 
+  * all of the information to save to the database needs to be added to exp_data. 
+  * exp_data is a javascript dictionary, which consists of [key, value] pairs
+  * To add a new pair to exp_data, do:
+  * exp_data.KEY = VALUE;
+  * you can see multiple examples of how this is done below
+  *
+  * when exp_data is completely built, it is passed as a paramter to saveData
+  * which actually writes the information to the database on Google App Engine
+  * 
+  * after writing the data, this function calls selectNextTrial to determine
+  * what the next type of trial should be
+  *
+  * to see how to retrieve all data, please look at the documentation in README.md
+  */
+  
   rt = new Date().getTime() - base_time;
 
   // all of the data from this trial will go into this object
@@ -128,7 +185,6 @@ function saveTestTrial() {
   exp_data.rt             = rt;
   exp_data.experiment     = "test_experiment_v1";
   exp_data.button_value   = response;
-  // SLIDER
   exp_data.slider_value = divSlider.slider('value');
 
   // print the data to console for debugging
@@ -137,16 +193,18 @@ function saveTestTrial() {
   // save trial data
   saveData(exp_data);
 
+  // determine what type of trial to run next
   selectNextTrial();
 }
 
 function selectNextTrial () {
-  // how many test trials to do per block
-  var maxTestTrial = 5;
-
-  // how many blocks to do total
-  var maxBlock = 2;
-
+  /*
+  * selectNextTrial determines based on the currTrial and currBlock variables
+  * what the next type of trial should be or if the experiment is done
+  * 
+  * the appropriate next function is called
+  */
+  
   // determine which section to go to next
   if(currTrial < maxTestTrial) {
     testTrial(); // next test trial
