72119ccfbf87cd1dd3b186ce2220b9f0820ab1ad
Drew Hendrickson <dhendrickson@gmail.com>
2014-06-27 11:34:30 +0930

    switch spacing convention

--- exp/js/init_exp.js
+++ exp/js/init_exp.js
@@ -1,28 +1,28 @@
-/*global $, document, console, alert */
+/*global $, document, window, console, alert, start */
 
 // this function runs automatically when the page is loaded
 $(document).ready(function () {
-    
-    // show an alert if user tries to navigate away from this page
-    window.onbeforeunload = function() {
-        return "Are you sure you want to leave this page?";
-    };
 
-    // load all scrips then begin the experiment
-    $.when(
+  // show an alert if user tries to navigate away from this page
+  window.onbeforeunload = function() {
+      return "Are you sure you want to leave this page?";
+  };
+
+  // load all scrips then begin the experiment
+  $.when(
 //        $.getScript( "http://code.jquery.com/ui/jquery-ui-git.js" ), // needed for the slider
-        $.getScript( "js/offline_testing_tools/jquery-ui-git.js" ), // needed for the slider
-        
-        
-        $.getScript( "/js/support_fcns.js" ),
-        $.getScript( "/js/exp_logic.js" ),
-        $.getScript( "/js/instructions.js" ),
-        $.getScript( "/js/demographics.js" ),
+    $.getScript( "js/offline_testing_tools/jquery-ui-git.js" ), // needed for the slider
 
-        $.Deferred(function( deferred ){
-            $( deferred.resolve );
-        })
-    ).done(function(){
-        start();
-    });
+
+    $.getScript( "/js/support_fcns.js" ),
+    $.getScript( "/js/exp_logic.js" ),
+    $.getScript( "/js/instructions.js" ),
+    $.getScript( "/js/demographics.js" ),
+
+    $.Deferred(function( deferred ){
+      $( deferred.resolve );
+    })
+  ).done(function(){
+    start();
+  });
 });
